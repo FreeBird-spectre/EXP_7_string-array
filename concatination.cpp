58a2c7bd8c0b382ee867aec1a5ef7223bacf466b
FreeBird-spectre <sethabir699@gmail.com>
2025-07-31 00:56:58 -0400

    Create concatination.cpp

---
+++ concatination.cpp
@@ -0,0 +1,26 @@
+\\ Abir Seth
+// ENTC A1
+
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+    string str1,str2,result;
+    cout<<"enter first string;";
+    cin>>str1;
+    cout<<"enter second string:";
+    cin>>str2;
+    result=str1+" "+str2;
+    cout<<result;
+    
+    
+
+    return 0;
+}
+
+enter first string;Abir
+enter second string:seth
+Abir seth
+
+=== Code Execution Successful ===
