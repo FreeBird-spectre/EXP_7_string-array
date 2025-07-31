e82827a5afe5193063ea0b34fcb8d4be5208e8fe
FreeBird-spectre <sethabir699@gmail.com>
2025-07-31 01:06:56 -0400

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,23 @@
+//Abir Seth
+//PRN-24070123003
+//entc a1
+
+#include<iostream>
+using namespace std;
+#include <string>
+int main()
+string str;
+cout<<"enter a string:";
+cin>>str;
+
+string reversed="";
+for (int i=str.length()-1;i
+
+
+
+
+
+
+
+
+  0
