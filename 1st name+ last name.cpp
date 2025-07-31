30c8017f9ed184b1177ae477c7746ade44ba61f6
FreeBird-spectre <sethabir699@gmail.com>
2025-07-31 00:54:00 -0400

    Create 1st name+ last name.cpp

---
+++ 1st name+ last name.cpp
@@ -0,0 +1,25 @@
+//Abi Seth
+//ENTC A1
+//PRN-24070123003
+#include<iostream>
+using namespace std;
+#include <string>
+int main()
+{
+    string str1;
+    string str2;
+    cout<<"enter your first name"<<endl;
+    cin>>str1;
+    cout<<"enter your last name"<<endl;
+    cin>>str2;
+
+return 0;
+}
+
+enter your first name
+AKSHAY
+enter your last name
+KHANNA
+
+
+=== Code Execution Successful ===
